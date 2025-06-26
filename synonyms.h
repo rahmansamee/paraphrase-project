218bdbbd04edbe2ac8ecc435dbecdb47af1f8de6
itxabid <85745236+itxabid@users.noreply.github.com>
2025-06-26 18:04:08 +0600

    Update synonyms.h

--- synonyms.h
+++ synonyms.h
@@ -13,19 +13,12 @@ typedef struct {
     int count;
 } Synonyms;
 
-extern Synonyms dictionary[];
-extern int DICT_SIZE;
-
-
 typedef struct {
     const char *original;
     const char *synonyms[MAX_SYNONYMS];
     int synonym_count;
     int word_len;
 } Phrase;
-
-extern Phrase phrases[];
-extern int PHRASE_DICT_SIZE;
 
 
 #endif // SYNONYMS_H
